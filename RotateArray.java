1e5a177e735d1741213960a4fc2c3c42186010cd
Rahul Kumar Gupta <75792872+rrkgupta01@users.noreply.github.com>
2025-03-19 15:48:37 +0530

    Update RotateArray.java

--- RotateArray.java
+++ RotateArray.java
@@ -1,34 +1,26 @@
 
 // Rotate an array by Kth Step.
-
+import java.util.*;
 public class RotateArray {
-    public static void rotate(int[] nums, int k) {
-        if (nums == null || nums.length == 0 || k <= 0) {
-            return;
-        }
-        int n = nums.length;
-        k = k % n; // In case k is greater than the length of the array
-        reverse(nums, 0, n - 1);
-        reverse(nums, 0, k - 1);
-        reverse(nums, k, n - 1);
-    }
-
-    private static void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;
+    public static void rotate(int nums[],int start,int end){
+        while (start<end){
+             int temp = nums[start];
+             nums[start] = nums[end];
+             nums[end] = temp;
+             start++;
+             end--;
         }
     }
-
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5, 6, 7};
-        int k = 3;
-        rotate(nums, k);
-        for (int num : nums) {
-            System.out.print(num + " ");
-        }
+        int arr[]={1,2,3,4,5};
+        int k=3;
+        int len=arr.length-1;
+        int s=0;
+        rotate(arr,s,len);
+        rotate(arr,s,k-1);
+        rotate(arr,k,len);
+        System.out.println(Arrays.toString(arr));
+        
     }
+
 }
